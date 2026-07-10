ca1bea3754bc95d6b088630676f12812fd486b34
TaH00R <bgmiseason002@gmail.com>
2026-07-10 10:43:49 +0530

    Created AppBar

--- lib/shared/widgets/appbar.dart
+++ lib/shared/widgets/appbar.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:gallery/core/constants/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -18,12 +20,33 @@ class GalleryAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       backgroundColor: AppColors.appBarBackground,
       foregroundColor: AppColors.appBarForeground,
-      title: Text(title,
-      style: GoogleFonts.graduate(
-        fontWeight: FontWeight.w600,
-        fontSize: 25,)
-       ),
       centerTitle: true,
+      elevation: 0,
+      // Center
+      title: Text(
+        title,
+        style: GoogleFonts.boogaloo(
+          fontSize: 30,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      flexibleSpace: Stack(
+    alignment: Alignment.center,
+    children: [
+      Positioned(
+        left: 0,
+        child: Image.asset("assets/images/branch.png", height: 150),
+      ),
+      Positioned(
+        right: 0,
+        child: Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.rotationY(pi),
+          child: Image.asset("assets/images/branch.png", height: 150),
+        ),
+      ),
+    ],
+  ),
     );
   }
 }
