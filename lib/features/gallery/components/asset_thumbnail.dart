75da7a4c370f2357a715c3c0531571c7276e1682
TaH00R <bgmiseason002@gmail.com>
2026-07-16 02:03:24 +0530

    Added feature to select multiple files at once

--- lib/features/gallery/components/asset_thumbnail.dart
+++ lib/features/gallery/components/asset_thumbnail.dart
@@ -1,61 +1,101 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:gallery/features/gallery/components/image_view.dart';
-import 'package:gallery/features/gallery/components/video_view.dart';
 import 'package:photo_manager/photo_manager.dart';
 
-class AssetThumbnail extends StatelessWidget {
+class AssetThumbnail extends StatefulWidget {
   final AssetEntity asset;
-  const AssetThumbnail({super.key, required this.asset});
+  final bool isSelected;
+  final VoidCallback onTap;
+  final VoidCallback onLongPress;
+
+  const AssetThumbnail({
+    super.key,
+    required this.asset,
+    required this.isSelected,
+    required this.onTap,
+    required this.onLongPress,
+  });
+
+  @override
+  State<AssetThumbnail> createState() => _AssetThumbnailState();
+}
+
+class _AssetThumbnailState extends State<AssetThumbnail> {
+  late final Future<Uint8List> _thumbnailFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _thumbnailFuture =
+        widget.asset.thumbnailData.then((value) => value!);
+  }
 
   @override
   Widget build(BuildContext context) {
-      return FutureBuilder<Uint8List>(
-        future: asset.thumbnailData.then((value) => value!),
-        builder: (context, snapshot) {
-          final bytes = snapshot.data;
-          if (bytes == null) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return InkWell(
-            onTap: () {
-              Navigator.push(context,
-              MaterialPageRoute(builder: (context) {
-                if(asset.type == AssetType.video){
-                  return VideoView(videoFile :  asset.file);
-                }
-              return ImageView(imageFile :  asset.file);
-            }));
-              },
-            child: Stack(
-              children: [
-                Positioned.fill(
-                  child: Image.memory(
-                    bytes,
-                    fit: BoxFit.cover,
-                  )),
-            
-                  if(asset.type == AssetType.video)
-                    Positioned(
-                      bottom: 4,
-                      right: 4,
-                      child: Container(
-                        padding: const EdgeInsets.all(2),
-                        color: Colors.black54,
-                        child: const Icon(
-                          Icons.videocam,
-                          color: Colors.white,
-                          size: 16,
-                        ),
-                      ),
-                ),
-              ],
-            ),
+    return FutureBuilder<Uint8List>(
+      future: _thumbnailFuture,
+      builder: (context, snapshot) {
+        final bytes = snapshot.data;
+
+        if (bytes == null) {
+          return const Center(
+            child: CircularProgressIndicator(),
           );
-        },
-      );
+        }
+
+        return GestureDetector(
+          onTap: widget.onTap,
+          onLongPress: widget.onLongPress,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Image.memory(
+                  bytes,
+                  fit: BoxFit.cover,
+                ),
+              ),
+
+              if (widget.isSelected)
+                Positioned.fill(
+                  child: Container(
+                    color: Colors.black38,
+                  ),
+                ),
+
+              if (widget.asset.type == AssetType.video)
+                Positioned(
+                  bottom: 4,
+                  right: 4,
+                  child: Container(
+                    padding: const EdgeInsets.all(2),
+                    color: Colors.black54,
+                    child: const Icon(
+                      Icons.videocam,
+                      color: Colors.white,
+                      size: 16,
+                    ),
+                  ),
+                ),
+
+              if (widget.isSelected)
+                const Positioned(
+                  top: 6,
+                  right: 6,
+                  child: CircleAvatar(
+                    radius: 12,
+                    backgroundColor: Colors.blue,
+                    child: Icon(
+                      Icons.check,
+                      color: Colors.white,
+                      size: 16,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
--- lib/features/gallery/gallery.dart
+++ lib/features/gallery/gallery.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gallery/features/gallery/components/asset_thumbnail.dart';
+import 'package:gallery/features/gallery/components/image_view.dart';
+import 'package:gallery/features/gallery/components/video_view.dart';
 import 'package:photo_manager/photo_manager.dart';
 
 class Gallery extends StatefulWidget {
@@ -13,11 +15,61 @@ class Gallery extends StatefulWidget {
 class _GalleryState extends State<Gallery> {
   List<AssetEntity> assets = [];
 
+  final Set<AssetEntity> selectedAssets = {};
+
+  bool selectionMode = false;
+
   Future<void> _fetchAssets() async {
-    final result = await PhotoManager.getAssetListRange(start: 0, end: 1000000);
+    final result = await PhotoManager.getAssetListRange(
+      start: 0,
+      end: 1000000,
+    );
+
+    if (!mounted) return;
+
     setState(() {
       assets = result;
     });
+  }
+
+  void _toggleSelection(AssetEntity asset) {
+    setState(() {
+      if (selectedAssets.contains(asset)) {
+        selectedAssets.remove(asset);
+
+        if (selectedAssets.isEmpty) {
+          selectionMode = false;
+        }
+      } else {
+        selectedAssets.add(asset);
+      }
+    });
+  }
+
+  void _startSelection(AssetEntity asset) {
+    setState(() {
+      selectionMode = true;
+      selectedAssets.add(asset);
+    });
+  }
+
+  void _openAsset(AssetEntity asset) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) {
+          if (asset.type == AssetType.video) {
+            return VideoView(videoFile: asset.file);
+          }
+
+          return ImageView(imageFile: asset.file);
+        },
+      ),
+    );
+  }
+
+  void _onGalleryChanged(MethodCall call) {
+    _fetchAssets();
   }
 
   @override
@@ -30,30 +82,49 @@ class _GalleryState extends State<Gallery> {
     PhotoManager.startChangeNotify();
   }
 
-  void _onGalleryChanged(MethodCall call) {
-    _fetchAssets();
-  }
-
   @override
   void dispose() {
     PhotoManager.removeChangeCallback(_onGalleryChanged);
     PhotoManager.stopChangeNotify();
+
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          crossAxisSpacing: 4.0,
-          mainAxisSpacing: 4.0,
+      body: GestureDetector(
+        // onPanStart:
+        // onPanUpdate:
+        // onPanEnd:
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 4,
+            mainAxisSpacing: 4,
+          ),
+          itemCount: assets.length,
+          itemBuilder: (_, index) {
+            final asset = assets[index];
+
+            return AssetThumbnail(
+              asset: asset,
+              isSelected: selectedAssets.contains(asset),
+
+              onTap: () {
+                if (selectionMode) {
+                  _toggleSelection(asset);
+                } else {
+                  _openAsset(asset);
+                }
+              },
+
+              onLongPress: () {
+                _startSelection(asset);
+              },
+            );
+          },
         ),
-        itemCount: assets.length,
-        itemBuilder: (_, index) {
-          return AssetThumbnail(asset: assets[index]);
-        },
       ),
     );
   }
